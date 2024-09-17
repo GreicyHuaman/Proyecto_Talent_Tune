5a6bc4f388942c2926ade0d42174c741a476d6db
Joao <goku_bh@hotmail.com>
2024-09-16 21:49:09 -0500

    Seguridad

---
+++ src/main/java/pe/edu/upc/talent_tune/securities/JWTRequest.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.talent_tune.securities;
+
+public class JWTRequest {
+}
---
+++ src/main/java/pe/edu/upc/talent_tune/serviceimplements/JwtUserDetailsService.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.talent_tune.serviceimplements;
+
+public class JwtUserDetailsService {
+}
