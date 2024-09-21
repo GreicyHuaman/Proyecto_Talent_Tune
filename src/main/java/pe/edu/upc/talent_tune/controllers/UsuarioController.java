ac0cca60123b352f07d475a46604db83e30f5381
Joao <goku_bh@hotmail.com>
2024-09-20 21:56:24 -0500

    V2

--- src/main/java/pe/edu/upc/talent_tune/controllers/UsuarioController.java
+++ src/main/java/pe/edu/upc/talent_tune/controllers/UsuarioController.java
@@ -2,6 +2,7 @@ package pe.edu.upc.talent_tune.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.talent_tune.dtos.UsuarioDTO;
 import pe.edu.upc.talent_tune.entities.Usuario;
@@ -17,6 +18,20 @@ public class UsuarioController {
     @Autowired
     private IUsuarioService uS;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @PostMapping
+    public void registrar(@RequestBody UsuarioDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Usuario u = m.map(dto, Usuario.class);
+        String encodedPassword = passwordEncoder.encode(u.getContrasenia());
+        u.setContrasenia(encodedPassword);
+        uS.insert(u);
+    }
+
+
     @GetMapping
     public List<UsuarioDTO> listarUsuarios() {
         return uS.list().stream().map(x -> {
@@ -25,12 +40,7 @@ public class UsuarioController {
         }).collect(Collectors.toList());
     };
 
-    @PostMapping
-    public void registrar(@RequestBody UsuarioDTO dto) {
-        ModelMapper m = new ModelMapper();
-        Usuario usuario = m.map(dto, Usuario.class);
-        uS.insert(usuario);
-    }
+
     @PatchMapping
     public void modificar (@RequestBody UsuarioDTO dto){
         ModelMapper m = new ModelMapper();
